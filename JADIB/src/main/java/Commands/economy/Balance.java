e366c1ab3a61f0cbb913768f660f50051e9444c9
DAflamingFOX <ffejsirrom.seb@gmail.com>
2021-04-28 09:57:48 -0500

    begining to start economy

---
+++ JADIB/src/main/java/Commands/economy/Balance.java
@@ -0,0 +1,17 @@
+package commands.economy;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import commandhandler.Command;
+import commandhandler.CommandData;
+import commandhandler.CommandExecutor;
+
+public class Balance implements CommandExecutor {
+
+    @Override
+    public void execute(CommandData data, ArrayList<Command> commands) throws IOException {
+                
+    }
+    
+}
---
+++ JADIB/src/main/java/Commands/economy/Economy.java
@@ -0,0 +1,5 @@
+package commands.economy;
+
+public class Economy {
+    
+}
