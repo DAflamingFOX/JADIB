f4b8a438a321226d9aef9cc881bf2b2de74ec201
DAflamingFOX <ffejsirrom.seb@gmail.com>
2021-04-30 12:21:09 -0500

    move Balance, start CommandDatabaseBackend

---
+++ JADIB/src/main/java/Commands/economy/CommandDatabaseBackend.java
@@ -0,0 +1,11 @@
+package commands.economy;
+
+public class CommandDatabaseBackend {
+    /**
+     * ! This class will be used for command access to databases,
+     * ! rather than Database.java, the issues is that there are quite
+     * ! alot of methods that I would not like to mix in with the SQLite
+     * ! methods; so instead they will go here.
+     * TODO: This is currently unused but will be fixed to work later
+     */
+}
