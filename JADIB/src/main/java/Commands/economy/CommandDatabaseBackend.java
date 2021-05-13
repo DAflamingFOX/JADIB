7e86ff6eeeb9fd3afdd0359a4a2747709f1cbedc
DAflamingFOX <ffejsirrom.seb@gmail.com>
2021-05-12 22:59:52 -0500

    general updates

--- JADIB/src/main/java/Commands/economy/CommandDatabaseBackend.java
+++ JADIB/src/main/java/Commands/economy/CommandDatabaseBackend.java
@@ -1,7 +1,8 @@
 package commands.economy;
 
+import java.sql.*;
+
 public class CommandDatabaseBackend {
-    // * Please run DatabaseFirstTimeSetup.java before attempting to run the bot!
     /**
      * ! This class will be used for command access to databases,
      * ! rather than Database.java, the issues is that there are quite
@@ -10,11 +11,74 @@ public class CommandDatabaseBackend {
      * TODO: This is currently unused but will be fixed to work later
      */
 
+     //* General Methods
+     /**
+      * 
+      * @param user_id the user id that will be added to the table
+      */
+     public static void addUser(String user_id) {
+        String updateStmt = "INSERT INTO master(user_id, balance) VALUES("+user_id+", 0);";
+        String database = "economy.db";
+
+        try {
+            Connection conn = Database.connect(database);
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(updateStmt);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+     }
+
      //* Balance Methods
+     public static String getBalance(String userId) {
+        String getBalanceQuery = "SELECT balance FROM master WHERE user_id == "+userId+";";
+        String database = "economy.db";
+        
+        try {
+            ResultSet rs = Database.sqlQuery(getBalanceQuery, database);
 
-     //* Beg Methods
+            if (rs.next()) {
+                String balance = rs.getString("balance");
+                return balance;
+            } else {
+                addUser(userId);
+                return getBalance(userId);
+            }
 
-     //* Vote Methods
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return "ERR!";
+    }
 
-     //* 
+    //* Vote Methods
+    public static long getLastVote(String user_id) {
+        String queryStmt = "SELECT voteTime FROM master WHERE user_id == "+user_id;
+        String database = "economy.db";
+        ResultSet rs = Database.sqlQuery(queryStmt, database);
+
+        try {
+            if (!rs.next()){ // checks to see if any value returns, will be empty if user is not in db
+                addUser(user_id);
+            }
+            return rs.getLong("lastVote");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return 0; // should never return this but it would work anyway.
+    }
+
+	public static void setLastVote(String user_id, long time) {
+        String updateStmt = "INSERT INTO master(lastVote) VALUES("+time+") WHERE user_id == "+user_id;
+        
+        Database.executeUpdateStatement(updateStmt, "economy.db");
+	}
+
+    //* Beg Methods
+
+
+    //* 
 }
--- JADIB/src/main/java/Commands/economy/Database.java
+++ JADIB/src/main/java/Commands/economy/Database.java
@@ -1,5 +1,8 @@
 package commands.economy;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -9,15 +12,18 @@ import java.sql.Statement;
 public class Database {
     final static String url = "jdbc:sqlite:JADIB/src/main/java/commands/economy/";
 
-    // * each function should check first that its able to, for instance connect, should make sure that there is a db to connect to, and if there isnt then make one.
     /**
-     * 
      * @param db the database name ending in .db to connect to
+     * if there is no db file, it will create one.
      * @return conn the SQLite db connection
      */
     public static Connection connect(String db) {
         Connection conn = null;
+        File location = new File(url.substring(12) + db);
         try {
+            if (!location.exists()) {
+                createDb(db);
+            }
             conn = DriverManager.getConnection(url + db);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
@@ -26,7 +32,45 @@ public class Database {
     }
 
     /**
+     * @param db the name of db to create
+     */
+    private static void createDb(String db) {
+        FileWriter file;
+        try {
+            file = new FileWriter(url.substring(12) + db);
+            file.flush();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    /**
      * 
+     * @param sql the sqlite statement to use
+     * @param db the database name ending in .db to connect to
+     */
+    public static void executeStatement(String sql, String db) {
+        try {
+            Connection conn = connect(db);
+            Statement stmt = conn.createStatement();
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+    }
+
+    public static void executeUpdateStatement(String sql, String db) {
+        try {
+            Connection conn = connect(db);
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
      * @param sql the SQLite query
      * @param db the database name ending in .db to connect to
      * @return rs The ResultSet from the SQLite query
@@ -45,25 +89,6 @@ public class Database {
         return null;
     }
 
-    public static String getBalance(String userid) {
-        String getBalanceQuery = "Select balance FROM Users WHERE userid == " + userid;
-        String database = "economy.db";
-        ResultSet rs = Database.sqlQuery(getBalanceQuery, database);
-        try {
-            /** if there is data for the user rs.next will return true
-            *   so if we use !rs.next() we will add them if they are not in db.
-            */
-            if (!rs.next()) {
-                Connection conn = connect(database);
-                Statement stmt = conn.createStatement();
-                stmt.executeUpdate("INSERT INTO Users(userid, balance) VALUES(" + userid +", 0)");
-                
-            }
-            return String.valueOf(rs.getInt("balance"));
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return "error!";
-    }
+    
 
 }
