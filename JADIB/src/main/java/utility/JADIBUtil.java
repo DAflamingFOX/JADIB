c515e88b436fa7fdd6c898a7bef9eab883d509a0
DAflamingFOX <ffejsirrom.seb@gmail.com>
2021-05-18 16:49:01 -0500

    update prefix

--- JADIB/src/main/java/utility/JADIBUtil.java
+++ JADIB/src/main/java/utility/JADIBUtil.java
@@ -11,7 +11,7 @@ import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.entity.user.User;
 
 public class JADIBUtil {
-    public static String prefix = "j-";
+    public static String prefix = "-";
     public static String avatarUrl = "https://cdn.discordapp.com/avatars/798780702463885322/7212eca92b0b89454ad9190a63f11e64.png?size=256";
     public static Color color = new Color(111, 78, 55);
     public static User getFirstMention(Message message) {
