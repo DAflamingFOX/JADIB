a5786291f89698eed2bd39ec413ff2124d659052
DAflamingFOX <ffejsirrom.seb@gmail.com>
2021-05-19 22:52:07 -0500

    created AddUserToDatabaseThread

---
+++ JADIB/src/main/java/threads/AddUserToDatabaseThread.java
@@ -0,0 +1,8 @@
+package threads;
+
+public class AddUserToDatabaseThread extends Thread{
+    public void run() {
+        //todo: cache and add users to database.
+        //* possibly need to revamp addUser method
+    }
+}
